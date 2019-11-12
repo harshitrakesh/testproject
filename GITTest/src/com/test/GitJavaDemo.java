93d6ffa073f4d98aa081e801411994acccd39b96
prashant <prashantpes@gmail.com>
2019-11-12 18:23:29 +0530

    githubrakesh

---
+++ GITTest/src/com/test/GitJavaDemo.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class GitJavaDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo");
+		System.out.println("First Step");
+		System.out.println("Second time after first commit");
+
+	}
+
+}
